b3136b323731cf3ec8856c2a908b2e7a8227b3d7
Tom Söderlund <tom@tomsoderlund.com>
2017-09-24 22:20:15 +0200

    Added Next.js frontend and an Index page with a list of kittens

--- index.js
+++ index.js
@@ -1,32 +1,42 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
-var router = express.Router();
-var app = express();
+const express = require('express');
+const router = express.Router();
+const server = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const next = require('next')
+const isDevelopment = process.env.NODE_ENV !== 'production'
+const app = next({ isDevelopment })
+const handle = app.getRequestHandler()
+const MONGO_URL = process.env.MONGO_URL || 'mongodb://localhost/next-event-guide'
+const PORT = 3001
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
+server.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
-app.use(bodyParser.json());
+server.use(bodyParser.json());
 /**
- * Uncomment these lines if you need cross origin domain request.
- */
-app.use(function(req, res, next) {
+* Uncomment these lines if you need cross origin domain request.
+*/
+server.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
-// mongoose.connect('mongodb://localhost/test');
-mongoose.connect(process.env.MONGO_URL);
+mongoose.connect(MONGO_URL);
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 
-app.use('/api', router);
-require('./routes/kittens')(app, router);
+server.use('/api', router);
+require('./routes/kittens')(server, router);
 
-app.listen(3001, function () {
-  console.log('Example app listening on port 3001!')
+server.get('*', (req, res) => {
+  return handle(req, res)
+})
+
+server.listen(PORT, function () {
+  console.log(`App running on http://localhost:${PORT}/\nAPI running on http://localhost:${PORT}/api/kittens`)
 });
