0fd92fc6c77e0dca0921cd38cfb3a5eb745da663
Tom Söderlund <tom@tomsoderlund.com>
2017-09-25 17:42:51 +0200

    Added nodemon for hot reloading on server changes

--- index.js
+++ index.js
@@ -5,38 +5,43 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const next = require('next')
-const isDevelopment = process.env.NODE_ENV !== 'production'
-const app = next({ isDevelopment })
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
 const handle = app.getRequestHandler()
+
 const MONGO_URL = process.env.MONGO_URL || 'mongodb://localhost/next-event-guide'
 const PORT = 3001
 
-// parse application/x-www-form-urlencoded
-server.use(bodyParser.urlencoded({ extended: false }));
+app.prepare().then(() => {
 
-// parse application/json
-server.use(bodyParser.json());
-/**
-* Uncomment these lines if you need cross origin domain request.
-*/
-server.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+  // Parse application/x-www-form-urlencoded
+  server.use(bodyParser.urlencoded({ extended: false }));
+  // Parse application/json
+  server.use(bodyParser.json());
 
-mongoose.connect(MONGO_URL);
+  // Allows for cross origin domain request:
+  server.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
+  // MongoDB
+  mongoose.connect(MONGO_URL);
+  var db = mongoose.connection;
+  db.on('error', console.error.bind(console, 'connection error:'));
 
-server.use('/api', router);
-require('./routes/kittens')(server, router);
+  // API routes
+  server.use('/api', router);
+  require('./routes/kittens')(server, router);
 
-server.get('*', (req, res) => {
-  return handle(req, res)
+  // Next.js route
+  server.get('*', (req, res) => {
+    return handle(req, res)
+  })
+
+  server.listen(PORT, function () {
+    console.log(`App running on http://localhost:${PORT}/\nAPI running on http://localhost:${PORT}/api/kittens`)
+  });
+
 })
-
-server.listen(PORT, function () {
-  console.log(`App running on http://localhost:${PORT}/\nAPI running on http://localhost:${PORT}/api/kittens`)
-});
